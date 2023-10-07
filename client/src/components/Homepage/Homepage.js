947e0c53215cccfe2f5946a8175d1420a5b16bb7
Lucas Von der Heyde <lucasvonderheyde@gmail.com>
2023-10-07 17:27:56 -0400

    added react and homepage

---
+++ client/src/components/Homepage/Homepage.js
@@ -0,0 +1,10 @@
+
+
+export default function Homepage(){
+    return (
+        <>
+            <h1>War of the Ring</h1>
+            <img src="https://www.boardgamequest.com/wp-content/uploads/2021/05/War-of-the-Ring-Game-Experience.jpg" />
+        </>
+    )
+}
